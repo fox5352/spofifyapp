fb062984f6a168dc98b86540b568229a75c4987a
Christopher <voschristopher53@gmail.com>
2024-10-23 00:41:14 +0200

    updared Card styling

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -31,8 +31,8 @@ export function Card({
   return (
     <Link to={`post/${id}`}>
       <div className="max-w-xs w-full group/card relative">
-        <div className="cursor-pointer overflow-hidden relative card h-96 rounded-md shadow-xl  max-w-sm mx-auto backgroundImage flex flex-col justify-between p-4 bg-zinc-950">
-          <div className="absolute w-full h-full top-0 left-0"></div>
+        <div className="cursor-pointer relative card h-96 rounded-md shadow-xl  max-w-sm mx-auto backgroundImage flex flex-col justify-between p-4 bg-zinc-950">
+          <div className="absolute w-full h-full top-0 left-0" />
           {/* header */}
           <div className="flex flex-row items-center space-x-4 z-10">
             <div className="flex flex-col">
@@ -42,26 +42,25 @@ export function Card({
               <p className="text-sm text-gray-400">{seasons} seasons</p>
             </div>
           </div>
-          <div className="absolute w-full">
-            <div className="absolute inset-0 bg-black opacity-20"></div>
+          {/* image */}
+          <div className="absolute px-1.5 left-0 w-fit h-fit">
+            <div className="absolute inset-0 bg-black opacity-45"></div>
             <img className="w-full h-auto" loading="lazy" src={image} alt="" />
           </div>
           {/* main content */}
-          <div className="text content">
+          <div className="text content relative">
+            <div className="relative z-20 flex gap-1">
+              {genreTitles &&
+                genreTitles.map((title) => (
+                  <GenreTag key={title} variant="filled">
+                    {title}
+                  </GenreTag>
+                ))}
+            </div>
             <p className="font-normal text-sm text-gray-50 relative z-10 my-4">
               {description.slice(0, 220)}
             </p>
           </div>
-        </div>
-        {/* shade on hover */}
-        <div className="absolute top-0 left-0 w-full h-full z-10 text-white transition duration-300 group-hover/card:bg-zinc-800 opacity-60" />
-        <div className="absolute z-20 top-0 left-0 hidden w-full h-full group-hover/card:flex  justify-evenly items-center">
-          {genreTitles &&
-            genreTitles.map((title) => (
-              <GenreTag key={title} variant="filled">
-                {title}
-              </GenreTag>
-            ))}
         </div>
       </div>
     </Link>
