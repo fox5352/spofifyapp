dd362d8208d5ac662f21b1b5e43acd3c193fdc77
Christopher <voschristopher53@gmail.com>
2024-10-22 04:33:15 +0200

    updated loading component

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,11 +1,12 @@
 import { BiLoaderCircle } from 'react-icons/bi'
 
-function Loading() {
+function Loading({ className }: { className?: string }) {
   return (
-    <div className="w-[100%] h-[100%] flex justify-center items-center">
-      <BiLoaderCircle className="w-1/2 h-1/2 text-purple-500 animate-spinslow text-primary" />
-    </div>
+    <BiLoaderCircle
+      className={`text-xl text-purple-500 animate-spinslow text-primary ${className}`}
+    />
   )
 }
+//
 
 export default Loading
