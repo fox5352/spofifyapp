b05b266d55abb6aa809b668f03ddbe6c5a9ba62b
Christopher <voschristopher53@gmail.com>
2024-10-22 04:35:27 +0200

    added PreviewFilterBar and dependent component

---
+++ src/pages/Home/components/TagButton.tsx
@@ -0,0 +1,38 @@
+import { MouseEvent, ReactNode, useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+import GenreTag from '../../../components/GenreTag'
+
+function TagButton({
+  param,
+  children,
+}: {
+  param: string
+  children: ReactNode
+}) {
+  const query = 'q'
+  const [isActive, setIsActive] = useState(false)
+  const [searchParams, setSearchParams] = useSearchParams()
+
+  useEffect(() => {
+    const isActiveParam = searchParams.get(query)
+    setIsActive(isActiveParam == param)
+  }, [searchParams, param])
+
+  const toggleParam = (event: MouseEvent) => {
+    event.stopPropagation()
+    if (searchParams.get(query) === param) {
+      searchParams.delete(query)
+    } else {
+      searchParams.set(query, param)
+    }
+    setSearchParams(searchParams)
+  }
+
+  return (
+    <button className="flex" onClick={toggleParam} type="button">
+      <GenreTag variant={isActive ? 'filled' : 'outline'}>{children}</GenreTag>
+    </button>
+  )
+}
+
+export default TagButton
