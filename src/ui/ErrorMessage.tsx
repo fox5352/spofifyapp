d4daae5304ed0e404efc8b16c1a976eccb2b0972
Christopher <christophervos522@gmail.com>
2024-11-05 12:47:27 +0000

    cleaned code

--- src/ui/ErrorMessage.tsx
+++ src/ui/ErrorMessage.tsx
@@ -1,20 +1,20 @@
-type textStize =
+type textSize =
   | 'text-sm'
   | 'text-md'
   | 'text-lg'
   | 'text-xl'
   | 'text-2xl'
   | 'text-3xl'
+  | 'text-4xl'
+  | 'text-5xl'
 
-function ErrorMessage({
-  message,
-  size,
-  className = '',
-}: {
+interface ErrorMessageProps {
   message: string
-  size: textStize
+  size: textSize
   className?: string
-}) {
+}
+
+function ErrorMessage({ message, size, className = '' }: ErrorMessageProps) {
   return (
     <div
       className={`w-full h-full flex justify-center items-center text-red-600 ${className}`}
